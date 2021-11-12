c198723dac833826cc1038d1edb67f65082532ef
PanBanan78 <oskar.matacz2001@gmail.com>
2021-11-12 18:15:27 +0000

    Testing

--- db/models.js
+++ db/models.js
@@ -22,6 +22,16 @@ const Customer = sequelize.define('Customer', {
     invoiceEmail: {
         type: DataTypes.STRING,
         allowNull: false
+    },
+
+    mailShotPermission: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+
+    whereDidYouHearAboutUs: {
+        type: DataTypes.STRING,
+        allowNull: true
     }
 })
 
@@ -63,10 +73,53 @@ const Contact = sequelize.define('Contact', {
     }
 })
 
+const Order = sequelize.define('Order', {
+    invoiceEmail: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    deliveryAddress: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    deliveryNotes: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+
+    generalNotes: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+
+    status: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
+
 Contact.belongsTo(Customer)
 Customer.hasMany(Contact)
 
+Order.belongsTo(Customer)
+Customer.hasMany(Order)
 
-  Customer.sync({force: true})
-  Contact.sync({force: true})
-  console.log("The table for the User model was just (re)created!")
+
+// Customer.sync({force: true})
+// Contact.sync({force: true})
+// Order.sync({force: true})
+
+// console.log("The table for the User model was just (re)created!")
+
+
+
+const foo = Customer.create({
+    name: 'foo',
+    type: 'clothing',
+    invoiceEmail: 'foo@foo.com',
+    mailShotPermission: false
+})
+
+console.log(foo)
